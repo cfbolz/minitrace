1ff3efe4439f26f4f17a88dbfd05b578c9a25960
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2022-07-13 16:45:21 +0200

    improvements

--- traceopt.py
+++ traceopt.py
@@ -1,14 +1,22 @@
 import pytest
 from dataclasses import dataclass
 
-# the idea of this file is to show how the union-find algorithm can be used in
-# an extremely simple local forward optimization pass. that pass will go over
-# the operations of a basic block once in the forward direction and returns a
-# new (shorter) list of optimized operations
+# the idea of this file is to show how a union-find data structure can be used
+# in an extremely simple local forward optimization pass. that pass will go
+# over the operations of a single basic block once in the forward direction and
+# returns a new (shorter) list of optimized operations
 
 # the approach is somewhat limited in that it is "local", ie only works for
 # basic blocks. It can be extended to work for extended basic blocks (making it
 # a superlocal optimizations).
+
+# the union find datastructure sorts the input operations in the basic blocks
+# into equivalence classes of operations that must all compute the same result.
+# As the various optimizations discover equalities, they call union. Every one
+# of these equivalence classes gets a representative, which is either an
+# operation that has to be emitted into the optimized basic block, or sometimes
+# even a constant.
+
 
 
 # let's start with the IR:
@@ -39,13 +47,11 @@ class Operation(Value):
 
     def make_equal_to(self, value : Value):
         # this is "union" in the union-find sense, but the direction is
-        # important! the representative of the union of operations must be
+        # important! the representative of the union of Operations must be
         # either a Constant or an operation that we know for sure is not
         # optimized away.
         self.find().forwarded = value
 
-    def eq(self, other):
-        return self == other # just by identity
       
 @dataclass(eq=False)
 class Constant(Value):
@@ -54,9 +60,7 @@ class Constant(Value):
     def find(self):
         return self
 
-    def eq(self, other):
-        return self.value == other.value
-      
+
 # helper function for construction Operation instances that wrap the arguments
 # into Constant if they aren't Values already, to make writing the examples
 # more convenient
@@ -112,10 +116,13 @@ def test_union_find():
 
 # _____________________________________________________________________
 
-# a basic block is a list of Operations. first, aconvenience function to print
+# a basic block is a list of Operations. first, a convenience function to print
 # basic blocks:
 
 def basicblock_to_str(l : list[Operation], varprefix : str = "var"):
+    # the implementation is not too important, look at the test below to see
+    # what the result looks like
+
     def arg_to_str(arg : Value):
         if isinstance(arg, Constant):
             return str(arg.value)
@@ -148,15 +155,19 @@ var0 = getarg(0)
 var1 = add(5, 4)
 var2 = add(var1, var0)"""
 
+    assert basicblock_to_str(bb, "x") == """\
+x0 = getarg(0)
+x1 = add(5, 4)
+x2 = add(x1, x0)"""
 
     # check that it interacts correctly with the union-find datastructure:
-    # let's say our still to-be-written optimizer did constant folding and
-    # replaced var2 with 9, and removed the op from the bb:
+    # if we unify var1 with the Constant 9, then var1 should be printed as 9
+    # when printing var2
     var1.make_equal_to(Constant(9))
     opt_bb = [var0, var2]
 
     # printing should see that replacement in the way it shows var2, due to the
-    # find call:
+    # find call in the implementation above:
     assert basicblock_to_str(opt_bb, "optvar") == """\
 optvar0 = getarg(0)
 optvar1 = add(9, optvar0)"""
@@ -164,14 +175,17 @@ optvar1 = add(9, optvar0)"""
 
 # _____________________________________________________________________
 
-# now comes the first actual optimization pass. for now, let's split the
+# Now comes the first actual optimization pass. For now, let's split the
 # optimizer into several passes, one for constant folding, one for common
 # subexpression elimination, one for strength reduction. later we will combine
 # them into a single pass
 
-# first a simple constant folding pass. I will show a buggy version of that
-# pass first. It has a problem that is related to why we need the union-find
-# data structure. We will fix it a bit further down.
+# Every pass has the same structure: we go over all operations in the basic
+# block in order and decide for each operation it can be removed.
+
+# First we'll look at a simple constant folding pass. I will show a buggy
+# version of that pass first. It has a problem that is related to why we need
+# the union-find data structure. We will fix it a bit further down.
 
 def constfold_buggy(bb: list[Operation]) -> list[Operation]:
     opt_bb = []
@@ -186,8 +200,10 @@ def constfold_buggy(bb: list[Operation]) -> list[Operation]:
                 # can constant-fold! that means we learned a new equality,
                 # namely that op is equal to a specific constant
                 op.make_equal_to(Constant(arg0.value + arg1.value))
-                continue # don't need to have the operation in the result
-        # otherwise the operation is put into the output list
+                # don't need to have the operation in the optimized basic block
+                continue
+        # otherwise the operation is not constant-foldable and put into the
+        # output list
         opt_bb.append(op)
     return opt_bb
 
@@ -212,7 +228,7 @@ def test_constfold_buggy_limitation():
 
     var0 = getarg(0)
     var1 = add(5, 4) # this is folded
-    var2 = add(var1, 10) # we want this folded too! but it doesn't work
+    var2 = add(var1, 10) # we want this folded too, but it doesn't work
     var3 = add(var2, var0)
 
     bb = [var0, var1, var2, var3]
@@ -221,18 +237,19 @@ def test_constfold_buggy_limitation():
 optvar0 = getarg(0)
 optvar1 = add(19, optvar0)"""
 
-    #  the test fails! the opt_bb printed output looks like this:
+    # why does the test fail? The opt_bb printed output looks like this:
     # optvar0 = getarg(0)
     # optvar1 = add(9, 10)
     # optvar2 = add(optvar1, optvar0)
 
-    # why is that? the problem is that when we optimize the second addition in
+    # The problem is that when we optimize the second addition in
     # constfold_buggy, the argument of that operation is an *Operation* not a
     # Constant, so constant-folding is not applied to the second add. However,
     # we have already learned that the argument var1 to the operation var2 is
     # equal to Constant(9). This information is stored in the union-find data
     # structure. So what we are missing are suitable find calls in
-    # constfold_buggy
+    # the constant folding pass, to make use of the previously learned
+    # equalities
 
 
 # here's the fixed version:
@@ -289,14 +306,14 @@ def cse(bb : list[Operation]) -> list[Operation]:
     for op in bb:
         # only do CSE for add here, but it generalizes
         if op.name == "add":
-            # really naive and quadratic implementation, a real one might use a
-            # hash-map of some kind. what we do is walk over the already
-            # emitted operations and see whether we emitted an add with the
-            # current arguments already.
-            prev_op = find_prev_add_op(op, opt_bb)
+            arg0 = op.args[0].find()
+            arg1 = op.args[1].find()
+            # Check whether we have emitted the same operation already
+            prev_op = find_prev_add_op(arg0, arg1, opt_bb)
             if prev_op is not None:
-                # optimize it away and replace it with the earlier result,
-                # which is an Operation that was already emitted to opt_bb
+                # if yes, we can optimize op away and replace it with the
+                # earlier result, which is an Operation that was already
+                # emitted to opt_bb
                 op.make_equal_to(prev_op)
                 continue
         opt_bb.append(op)
@@ -311,13 +328,17 @@ def eq_value(val0, val1):
     return val0 is val1
 
 
-def find_prev_add_op(op : Operation, opt_bb : list[Operation]) -> Operation | None:
-    arg0 = op.args[0].find()
-    arg1 = op.args[1].find()
+def find_prev_add_op(arg0 : Value, arg1 : Value, opt_bb : list[Operation]) -> Operation | None:
+    # Really naive and quadratic implementation. what we do is walk over the
+    # already emitted operations and see whether we emitted an add with the
+    # current arguments already. A real implementation might use a hashmap of
+    # some kind, or at least only look at a limited window of instructions.
     for opt_op in opt_bb:
         if opt_op.name != "add":
             continue
-        if eq_value(opt_op.args[0].find(), arg0) and eq_value(opt_op.args[1].find(), arg1):
+        # It's important to call find here, for the same reason why we needed
+        # it in constfold.
+        if eq_value(arg0, opt_op.args[0].find()) and eq_value(arg1, opt_op.args[1].find()):
             return opt_op
     return None
 
@@ -345,8 +366,8 @@ optvar4 = add(optvar3, optvar3)"""
 
 # now we have a pass that replaces Operations with Constants and one that
 # replaces Operations with previously existing Operations. Let's now do one
-# that replaces Operations by newly invented Operations, a simple strength
-# reduction
+# final pass that replaces Operations by newly invented Operations, a simple
+# strength reduction
 
 def strength_reduce(bb: list[Operation]) -> list[Operation]:
     opt_bb = []
@@ -374,7 +395,8 @@ optvar1 = lshift(optvar0, 1)"""
 
 # _____________________________________________________________________
 
-# let's combine the passes
+# Let's combine the passes into one single pass, so that we are going over all
+# the operations only exactly once.
 
 def optimize(bb: list[Operation]) -> list[Operation]:
     opt_bb = []
@@ -383,18 +405,27 @@ def optimize(bb: list[Operation]) -> list[Operation]:
         if op.name == "add":
             arg0 = op.args[0].find()
             arg1 = op.args[1].find()
+
+            # constant folding
             if isinstance(arg0, Constant) and isinstance(arg1, Constant):
-                # constant fold
                 op.make_equal_to(Constant(arg0.value + arg1.value))
                 continue
+
             # cse
-            prev_op = find_prev_add_op(op, opt_bb)
+            prev_op = find_prev_add_op(arg0, arg1, opt_bb)
             if prev_op is not None:
                 op.make_equal_to(prev_op)
                 continue
-            # strength reduce
 
-            # arithmetic simplification: a + 0 => a
+            # strength reduce
+            if arg0 is arg1: # x + x turns into x << 1
+                newop = lshift(arg0, 1)
+                opt_bb.append(newop)
+                op.make_equal_to(newop)
+                continue
+
+            # and while we are at it, let's do some arithmetic simplification:
+            # a + 0 => a
             if eq_value(arg0, Constant(0)):
                 op.make_equal_to(arg1)
                 continue
